9c7cbe6c6b1eafb1005c1963d8bb03e06f33b7a0
LauraIaia <laura.iaia2000@gmail.com>
2021-09-02 23:51:56 +0200

    errore_storico

--- routes/storico.js
+++ routes/storico.js
@@ -5,10 +5,19 @@ var router = express.Router();
 /* GET home page. */
 router.get('/storico', function(req, res, next) {
   var db = new DB();
+    if(!req.user){
+      //error
+      res.render('errore_storico', {title: 'errore_storico' });
+      return;
+    }
+  const idUtente = req.user.userProfile.id;
   db.getOrders(idUtente, (error, results, fields) => {
     if(error) {
       next(error);
       return;
     };
+    res.render('storico', {results, title: 'Storico' });
 });
 });
+
+module.exports = router;
